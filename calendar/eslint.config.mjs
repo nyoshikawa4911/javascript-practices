b75655ccf856a021359df1a4583e8035d53def4e
nyoshikawa4911 <nyoshikawa4911@gmail.com>
2025-03-28 16:53:14 +0900

    eslint-plugin-vitestをインストール、設定を追加

--- calendar/eslint.config.mjs
+++ calendar/eslint.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "eslint/config";
 import globals from "globals";
 import js from "@eslint/js";
 import eslintConfigPrettier from "eslint-config-prettier/flat";
+import vitest from "@vitest/eslint-plugin";
 
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs}"] },
@@ -12,4 +13,16 @@ export default defineConfig([
     extends: ["js/recommended"],
   },
   eslintConfigPrettier,
+  {
+    files: ["__tests__/**"],
+    plugins: { vitest },
+    rules: {
+      ...vitest.configs.recommended.rules,
+    },
+    languageOptions: {
+      globals: {
+        ...vitest.environments.env.globals,
+      },
+    },
+  },
 ]);
